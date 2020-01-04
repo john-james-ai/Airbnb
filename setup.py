f4efe2c3bfd197b10cdea1bea1bc2b8349c0335d
John James <jjames@decisionscients.com>
2020-01-04 18:58:18 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Airbnb Occupancy Analysis and Predictive Analytics',
+    author='John James',
+    license='BSD-3',
+)
